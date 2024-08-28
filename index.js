809afccefd25960dc84697e35c4282b25b2a66a0
Galangyp <galangy11@gmail.com>
2024-08-28 20:40:13 +0700

    Bisa input

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const kataRoute = require("./routes/kata.routes.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("bisa bang");
