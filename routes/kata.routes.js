767ade0e44f54b6e50dff27d17c4941246202f81
Galangyp <galangy11@gmail.com>
2024-08-29 17:08:55 +0700

    cari kata

--- routes/kata.routes.js
+++ routes/kata.routes.js
@@ -20,6 +20,18 @@ router.get("/kata", async (req, res) => {
   }
 });
 
+router.get("/kata/:cariKata", async (req, res) => {
+  try {
+    const { cariKata } = req.params;
+    const kata = await Kata.find({
+      $or: [{ indonesia: cariKata }, { sunda: cariKata }],
+    });
+    res.status(200).json(kata);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.get("/kata/:id", async (req, res) => {
   try {
     const { id } = req.params;
